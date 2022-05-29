3ab140f621da0f2edee6b7c59ae37059c0546f5c
rubenvazquezvalero <ruvaz.pap@gmail.com>
2022-05-29 16:38:17 +0200

    cambios

--- gametracker/database/migrations/2022_05_16_172113_create_games_table.php
+++ gametracker/database/migrations/2022_05_16_172113_create_games_table.php
@@ -16,6 +16,7 @@ class CreateGamesTable extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug');
             $table->text('description');
             $table->timestamps();
         });
